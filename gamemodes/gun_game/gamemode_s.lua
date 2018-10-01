f2431d0ac9bb0b9e1e45b8c4ca80f8831a7ee7c1
Shayan <hello@shayandoust.me>
2018-10-01 20:57:20 +0100

    Experimental tests

--- gamemodes/gun_game/gamemode_s.lua
+++ gamemodes/gun_game/gamemode_s.lua
@@ -1,7 +1,14 @@
 local GunLevels = {}
+local PlayerList = {}
+local Session_
+local SessionActive = false
+local SessionRunnable = true
+
+RegisterNetEvent("Gamemode:UpdatePlayers:4")
+RegisterNetEvent("Gamemode:Heartbeat:4")
 
 AddEventHandler("Gamemode:Start:4", function()
-    for i=0, GetNumPlayerIndices()-1 do
+    for i=0, GetNumPlayerIndices() - 1 do
         local player = GetPlayerFromIndex(i)
         GunLevels[player] = 1
     end
@@ -11,4 +18,12 @@ AddEventHandler("baseevents:onPlayerKilled", function(killerid, data)
     GunLevels[killerid] = GunLevels[killerid] + 1
     TriggerClientEvent("gun_game:UpGunLevel", killerid, GunLevels[killerid])
     TriggerClientEvent("gun_game:UpdateLevels", -1, GunLevels)
+end)
+
+AddEventHandler("Gamemode:UpdatePlayers:4", function(Operation, Player)
+    if Operation == "Append" then
+        PlayerList[#PlayerList + 1] = Player
+    elseif Operation == "Fetch" then
+        return PlayerList
+    end
 end)
